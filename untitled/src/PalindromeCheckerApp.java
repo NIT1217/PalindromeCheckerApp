a0a84fb11001582a776ed3c3c5a958f28415c832
Nitish <nk3553@srmist.edu.in>
2026-03-09 08:30:51 +0530

    cahnged done

---
+++ untitled/src/PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+
+    }
+}
